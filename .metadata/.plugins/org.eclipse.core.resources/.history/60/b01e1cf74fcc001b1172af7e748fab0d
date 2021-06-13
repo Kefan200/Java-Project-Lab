fcd83c2e564f4db3a32d1d9e6e6eb5eab9bdb3aa
Kefan200 <mykefan23@gmail.com>
2021-06-13 21:38:11 +0700

    add insert to menu and menucontroller

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/60/b01e1cf74fcc001b1172af7e748fab0d
@@ -0,0 +1,12 @@
+import java.util.Vector;
+
+public class MenuController {
+
+	public MenuController() {
+		// TODO Auto-generated constructor stub
+	}
+	public static Vector<Menu> getAllMenus(){
+		Menu menu=new Menu();
+		return menu.getAll();
+	}
+}
--- Project Lab OOAD/src/MenuController.java
+++ Project Lab OOAD/src/MenuController.java
@@ -9,4 +9,14 @@ public class MenuController {
 		Menu menu=new Menu();
 		return menu.getAll();
 	}
+	public static boolean insertMenu(String role, String name, String username, String status, int salary) {
+		Menu menu=new Menu();
+		menu.setRole(role);
+		menu.setName(name);
+		menu.setUsername(username);
+		menu.setStatus(status);
+		menu.setSalary(salary);
+		
+		return menu.insert();
+	}
 }
